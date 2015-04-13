a1a516f08b69db6d3e36967d9fe4dc502926eb6f
colinjlacy <colin@webcake.co>
2015-04-12 21:20:47 -0500

    [create-editor] adds the functionality to make a static file, plus add to an index array stored in a json file

---
+++ edit/webapp/post/postCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by colinjlacy on 4/11/15.
+ */
+angular.module('flatironEdit')
+.controller('postCtrl', function($scope) {
+
+	});
---
+++ edit/webapp/post/savePostDir.js
@@ -0,0 +1,35 @@
+/**
+ * Created by colinjlacy on 4/11/15.
+ */
+angular.module('flatironEdit')
+.directive('flSavePost', function($http, apiSrvc) {
+		return {
+			restrict: "A",
+			link: function(scope, elem) {
+				elem.on('click', function(e) {
+					e.preventDefault();
+
+					var postObj = {};
+
+					if(!scope.createdOn) {
+						postObj.createdOn = new Date();
+						postObj.revisedOn = angular.copy(postObj.createdOn);
+					} else {
+						postObj.createdOn = scope.createdOn;
+						postObj.revisedOn = new Date();
+					}
+
+					// get content from the thing
+					postObj.postTitle = scope.postTitle;
+					postObj.postSubHeading = scope.postSubHeading;
+					postObj.postContent = scope.postContent;
+					postObj.postExcerpt = scope.postExcerpt;
+
+					apiSrvc.postData('http://localhost:8080/api/post/', postObj).then(function(res){ // todo: add URL to a config
+						console.log(res);
+					});
+
+				});
+			}
+		}
+	});

49e2778339ad51e9f2434153c27c26d479f5659f
colinjlacy <colin@webcake.co>
2015-04-25 08:02:18 -0500

    [restrcture] wraps up the first big restructure

--- webapp/post/edit/edit.js
+++ webapp/post/edit/edit.js
@@ -2,7 +2,7 @@
  * Created by colinjlacy on 4/11/15.
  */
 angular.module('flatiron.edit', [])
-.directive('flSavePost', function(apiSrvc) {
+.directive('fiSavePost', function(apiSrvc) {
 		return {
 			restrict: "A",
 			link: function(scope, elem) {
